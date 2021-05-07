487a394b4eee8183c1b4fce58add828941f937ba
Arkadeep De <arkadeepde142@gmail.com>
2021-05-07 21:27:58 +0530

    Done

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,105 @@
 mod singletonbuilder;
-
 use autoclicker::Autoclicker;
-use rdev::{listen, Event, EventType, Key};
+use druid::text::format::{Formatter, Validation, ValidationError};
+use druid::text::selection::Selection;
+use druid::widget::{Button, CrossAxisAlignment, Flex, MainAxisAlignment, TextBox};
+use druid::{
+    AppDelegate, AppLauncher, Data, Lens, LocalizedString, PlatformError, Widget, WidgetExt,
+    WindowDesc, DelegateCtx, WindowId, Env
+};
+use singletonbuilder::singleton;
 
-fn main() {
-    let global = singleton();
-    let mut autoclicker = global.autoclicker.lock().unwrap();
-    *autoclicker = Autoclicker::new(200);
-    listen(callback);
+// const VERTICAL_WIDGET_SPACING: f64 = 20.0;
+const TEXT_BOX_WIDTH: f64 = 200.0;
+const WINDOW_TITLE: LocalizedString<Model> = LocalizedString::new("Autoclicker");
+#[derive(Clone, Data, Lens)]
+struct Model {
+    delay: u64,
 }
 
-fn _ui_builder() {}
+struct DelayFormatter;
 
-use singletonbuilder::singleton;
-fn callback(event: Event) {
-    match event.event_type {
-        EventType::KeyPress(Key::KeyW) => {
-            let global = singleton();
-            let mut autoclicker = global.autoclicker.lock().unwrap();
+impl Formatter<u64> for DelayFormatter {
+    fn format(&self, value: &u64) -> String {
+        value.to_string()
+    }
 
-            if !autoclicker.running() {
-                autoclicker.start().unwrap()
-            } else {
-                autoclicker.stop().unwrap()
+    fn validate_partial_input(&self, input: &str, _sel: &Selection) -> Validation {
+        match input.parse::<u64>() {
+            Ok(_) => Validation::success(),
+            Err(err) => {
+                if input == "" {
+                    return Validation::success();
+                }
+                Validation::failure(err)
             }
         }
-        EventType::KeyPress(Key::KeyS) => {
-            println!("Exiting");
-            std::process::exit(0);
-        }
-        _ => {}
     }
+
+    fn value(&self, input: &str) -> Result<u64, ValidationError> {
+        match input.parse::<u64>() {
+            Ok(value) => {
+                println!("{}", value);
+                return Ok(value);
+            }
+            Err(err) => {
+                return Err(ValidationError::new(err));
+            }
+        }
+    }
+}
+
+struct Delegate;
+
+impl AppDelegate<Model> for Delegate {
+    fn window_removed(
+        &mut self,
+        _id: WindowId,
+        _data: &mut Model,
+        _env: &Env,
+        _ctx: &mut DelegateCtx<'_>,
+    ) {
+        std::process::exit(0);
+    }
+}
+
+fn main() -> Result<(), PlatformError> {
+    singleton();
+    let main_window = WindowDesc::new(_ui_builder)
+        .title(WINDOW_TITLE)
+        .resizable(false)
+        .window_size((400., 200.));
+
+    let model = Model { delay: 200 };
+    AppLauncher::with_window(main_window)
+        .delegate(Delegate {})
+        // .use_simple_logger()
+        .launch(model)
+}
+
+fn _ui_builder() -> impl Widget<Model> {
+    let textbox = TextBox::new()
+        .with_placeholder("Delay")
+        .with_formatter(DelayFormatter {})
+        .update_data_while_editing(true)
+        .lens(Model::delay)
+        .fix_width(TEXT_BOX_WIDTH)
+        .padding(5.0);
+    let button = Button::new("Set Delay")
+        .on_click(|_ctx, data: &mut Model, _env| {
+            _update_delay(data.delay);
+        })
+        .padding(5.0);
+
+    Flex::row()
+        .main_axis_alignment(MainAxisAlignment::Center)
+        .cross_axis_alignment(CrossAxisAlignment::Center)
+        .with_child(textbox)
+        .with_default_spacer()
+        .with_child(button)
+}
+
+fn _update_delay(delay: u64) {
+    let bot = singleton();
+    *(bot.autoclicker.lock().unwrap()) = Autoclicker::new(delay);
 }
--- src/singletonbuilder.rs
+++ src/singletonbuilder.rs
@@ -2,6 +2,7 @@ use autoclicker::Autoclicker;
 use std::mem;
 use std::sync::{Arc, Mutex, Once};
 
+use rdev::{listen, Event, EventType, Key};
 #[derive(Clone)]
 pub struct SingletonReader {
     // Since we will be used in many threads, we need to protect
@@ -16,12 +17,35 @@ pub fn singleton() -> SingletonReader {
         ONCE.call_once(|| {
             // Make it
             let singleton = SingletonReader {
-                autoclicker: Arc::new(Mutex::new(Autoclicker::new(200))),
+                autoclicker: Arc::new(Mutex::new(Autoclicker::new(1))),
             };
             // Put it in the heap so it can outlive this call
             SINGLETON = mem::transmute(Box::new(singleton));
+            std::thread::spawn(move || {
+                listen(callback);
+            });
         });
         // Now we give out a copy of the data that is safe to use concurrently.
         (*SINGLETON).clone()
+    }
+}
+
+fn callback(event: Event) {
+    match event.event_type {
+        EventType::KeyPress(Key::KeyW) => {
+            let global = singleton();
+            let mut autoclicker = global.autoclicker.lock().unwrap();
+
+            if !autoclicker.running() {
+                autoclicker.start().unwrap()
+            } else {
+                autoclicker.stop().unwrap()
+            }
+        }
+        EventType::KeyPress(Key::KeyS) => {
+            println!("Exiting");
+            std::process::exit(0);
+        }
+        _ => {}
     }
 }
