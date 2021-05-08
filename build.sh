4499f1eda4334b16f3cf13984e649de9c2e67b09
Arkadeep De <arkadeepde142@gmail.com>
2021-05-08 10:53:49 +0530

    Release added

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 export MACOSX_DEPLOYMENT_TARGET=10.7;
 cargo build --release;
+mkdir release/temp;
+cp target/release/autoclicker release/temp/;
+cp README.md LICENSE release/temp/;
+tar czvf release/autoclicker.tar.gz release/temp/;
+rm -rf release/temp;
